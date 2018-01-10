3039eba03a8e9894444b3597e32bd8f4c6b9863b
vsingh <vsingh@palantir.com>
2018-01-10 15:49:51 -0800

    making some ch-ch-ch-changes

--- app/src/main/java/com/example/vsingh/sunset_setup/MainActivity.java
+++ app/src/main/java/com/example/vsingh/sunset_setup/MainActivity.java
@@ -3,11 +3,26 @@ package com.example.vsingh.sunset_setup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+    private String name = "NAME";
+    private Date date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    MainActivity (String name, Date date) {
+        this.name = name;
+        this.date = date;
+    }
+
+    public static void initialize (MainActivity mainActivity) {
+        mainActivity.date = new Date();
+        mainActivity.name = new Date().toString();
     }
 }
